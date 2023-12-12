b992f1f38b4b82c277623b58e3710b67b1a7148b
Jack Lingle <captainjack0818@gmail.com>
2023-12-11 19:23:34 -0500

    finish drive subsystem

--- src/main/java/frc/robot/subsystems/DifferentialDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DifferentialDriveSubsystem.java
@@ -9,6 +9,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants.DrivetrainConstants;
 
 
@@ -19,44 +21,37 @@ public class DifferentialDriveSubsystem extends SubsystemBase {
   private final WPI_VictorSPX backLeft;
   private final WPI_VictorSPX backRight;
 
+  private final MotorControllerGroup leftDrive;
+  private final MotorControllerGroup rightDrive;
+
   private final DifferentialDrive drive;
+
+  private double forward;
+  private double rotation;
 
   public DifferentialDriveSubsystem() {
     frontLeft = new WPI_VictorSPX(DrivetrainConstants.frontLeftId);
     frontRight = new WPI_VictorSPX(DrivetrainConstants.frontRightId);
     backLeft = new WPI_VictorSPX(DrivetrainConstants.backLeftId);
     backRight = new WPI_VictorSPX(DrivetrainConstants.backRightId);
+    
+    leftDrive = new MotorControllerGroup(frontLeft, backLeft);
+    rightDrive = new MotorControllerGroup(frontRight, backRight);
 
-    drive = new DifferentialDrive(new MotorControllerGroup(frontLeft, backLeft), new MotorControllerGroup(frontRight, backRight));
+    drive = new DifferentialDrive(leftDrive, rightDrive);
+
+    forward = 0;
+    rotation = 0;
   }
-
-  /**
-   * Example command factory method.
-   *
-   * @return a command
-   */
-  public Command exampleMethodCommand() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          /* one-time action goes here */
-        });
+  public void arcadeDrive(double forwardInput, double rotationInput) {
+    forward = forwardInput;
+    rotation = rotationInput;
+    drive.arcadeDrive(forwardInput, rotationInput);
   }
-
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
-  }
-
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("forward output", forward);
+    SmartDashboard.putNumber("Rotation output", rotation);
   }
 
   @Override
