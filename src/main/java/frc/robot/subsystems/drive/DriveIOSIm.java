cbcedf60c3752213006e2c23696879004dba2c86
Jack Lingle <captainjack0818@gmail.com>
2023-12-15 07:43:17 -0500

    add update inputs method for sim class

--- src/main/java/frc/robot/subsystems/drive/DriveIOSIm.java
+++ src/main/java/frc/robot/subsystems/drive/DriveIOSIm.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
 import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim.KitbotGearing;
 import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim.KitbotMotor;
 import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim.KitbotWheelSize;
+import frc.robot.subsystems.drive.DriveIO.DriveIOInputs;
 
 public class DriveIOSIm {
     private DifferentialDrivetrainSim driveSim = DifferentialDrivetrainSim.createKitbotSim(KitbotMotor.kDualCIMPerSide, KitbotGearing.k10p71, KitbotWheelSize.kSixInch, null);
@@ -15,5 +16,10 @@ public class DriveIOSIm {
         leftAppliedVolts = MathUtil.clamp(leftVolts, -12.0, 12.0);
         rightAppliedVolts = MathUtil.clamp(rightVolts, -12.0, 12.0);
         driveSim.setInputs(leftAppliedVolts, rightAppliedVolts);
+    }
+    public void uodateInputs(DriveIOInputs inputs) {
+        driveSim.update(0.02);
+        inputs.leftSpeed = leftAppliedVolts / 12.0;
+        inputs.rightSpeed = rightAppliedVolts / 12.0;
     }
 }
