02c4a60ecd3235dff6b648284d2f0c8bd560820e
Jack Lingle <captainjack0818@gmail.com>
2023-12-20 18:43:01 -0500

    add module io interface

---
+++ src/main/java/frc/robot/subsystems/swerve/ModuleIO.java
@@ -0,0 +1,28 @@
+package frc.robot.subsystems.swerve;
+
+import org.littletonrobotics.junction.AutoLog;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+
+public interface ModuleIO {
+    @AutoLog
+    public static class ModuleIOInputs {
+        // rotation inputs
+        public double rotationVoltage = 0.0;
+        public Rotation2d turnAbsolutePosition = new Rotation2d();
+        public Rotation2d turnPosition = new Rotation2d();
+        public double turnVelRadiansPerSec = 0.0;
+        public double[] turnCurrentAmps = new double[] {};
+
+        // drive inputs
+        public double driveVoltage = 0.0;
+        public double driveVelRadiansPerSec = 0.0;
+        public double drivePositionRad = 0.0;
+        public double[] driveCurrentAmps = new double[] {};
+    }
+    public default void updateInputs(ModuleIOInputs inputs) {}
+    public default void setDriveVoltage(double volts) {}
+    public default void setTurnVoltage(double volts) {}
+    public default void setDriveBrakeMode(boolean braked) {}
+    public default void setTurnBrakeMode(boolean braked) {}
+}
