2f671e05cdcbdb4ea878e4bef87beeb7ff5a8695
Jack Lingle <captainjack0818@gmail.com>
2023-12-21 13:32:07 -0500

    update constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,10 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.util.Units;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -44,5 +48,42 @@ public final class Constants {
     public static final int frontRightId = 2;
     public static final int backLeftId = 3;
     public static final int backRightId = 4;
+  }
+  public static final class SwerveDriveConstants {
+
+        public static final double trackWidth = Units.inchesToMeters(20.4);
+        public static final double wheelBase = Units.inchesToMeters(22.4);
+
+        public static final int frontLeftDriveID = 1;
+        public static final int frontRightDriveID = 3;
+		    public static final int backRightDriveID = 5;
+		    public static final int backLeftDriveID = 7;
+		
+        public static final int frontLeftRotationMotorID = 2;
+        public static final int frontRightRotationMotorID = 4;
+        public static final int backRightRotationMotorID = 6;
+        public static final int backLeftRotationMotorID = 8;
+
+        public static final int frontLeftRotationEncoderID = 1; 
+        public static final int frontRightRotationEncoderID = 2; 
+        public static final int backRightRotationEncoderID = 3;
+        public static final int backLeftRotationEncoderID = 4; 
+
+        public static final double frontLeftAngleOffset = -2.3930100291016;
+        public static final double frontRightAngleOffset = 2.560213934981135; 
+        public static final double backRightAngleOffset = 2.389942067525829;
+        public static final double backLeftAngleOffset = -0.582912699396544;
+
+		public static final double autoDrivePercent = 0.6;
+        public static final double driveWheelGearReduction = 6.12; // placeholder
+        public static final double wheelRadiusM = Units.inchesToMeters(2);
+
+        public static final SwerveDriveKinematics kinematics = 
+            new SwerveDriveKinematics(
+                new Translation2d(trackWidth / 2.0, wheelBase / 2.0),
+                new Translation2d(trackWidth / 2.0, -wheelBase / 2.0),
+                new Translation2d(-trackWidth / 2.0, wheelBase / 2.0),
+                new Translation2d(-trackWidth / 2.0, -wheelBase / 2.0)
+    );
   }
 }
---
+++ src/main/java/frc/robot/subsystems/swerve/ModuleIOSparkMax.java
@@ -0,0 +1,6 @@
+package frc.robot.subsystems.swerve;
+
+
+public class ModuleIOSparkMax implements ModuleIO {
+    
+}
