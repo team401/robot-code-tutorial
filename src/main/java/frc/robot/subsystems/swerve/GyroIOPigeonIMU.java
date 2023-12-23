16b65167c5ef2a24c3dd6e6ae65b2da5a3106e42
Jack Lingle <captainjack0818@gmail.com>
2023-12-23 16:41:11 -0500

    add new inputs to method for pigeon and add offsets

--- src/main/java/frc/robot/subsystems/swerve/GyroIOPigeonIMU.java
+++ src/main/java/frc/robot/subsystems/swerve/GyroIOPigeonIMU.java
@@ -8,10 +8,20 @@ import frc.robot.Constants.SwerveDriveConstants;
 public class GyroIOPigeonIMU implements GyroIO{
     private PigeonIMU gyro;
 
+    private double degYawOffset = 0;
+    private double degPitchOffset = 0;
+     private double degRollOffset = 0;
+
     public GyroIOPigeonIMU () {
         gyro = new PigeonIMU(SwerveDriveConstants.PigeonIMUID);
+
+        degYawOffset = gyro.getYaw();
+        degRollOffset = gyro.getRoll();
+        degPitchOffset = gyro.getPitch();
     }
     public void updateInputs (GyroIOInputs inputs) {
-        inputs.yawPosition = Rotation2d.fromDegrees(gyro.getYaw());
+        inputs.yawPosition = Rotation2d.fromDegrees(gyro.getYaw() - degYawOffset);
+        inputs.pitchPosition = Rotation2d.fromDegrees(gyro.getPitch() - degPitchOffset);
+        inputs.rollPosition = Rotation2d.fromDegrees(gyro.getRoll() - degRollOffset);
     }
 }
