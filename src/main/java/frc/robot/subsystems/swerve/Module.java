f0244c53c19b668429d2104aa0e4a282964a43df
Jack Lingle <captainjack0818@gmail.com>
2024-01-01 16:24:26 -0500

    add periodic method for modules to call in drive class

--- src/main/java/frc/robot/subsystems/swerve/Module.java
+++ src/main/java/frc/robot/subsystems/swerve/Module.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems.swerve;
 
+import org.littletonrobotics.junction.Logger;
+
 public class Module {
     private final ModuleIO io;
     private final int index;
@@ -8,5 +10,10 @@ public class Module {
     public Module (ModuleIO io, int index) {
         this.io = io;
         this.index = index;
+    }
+
+    public void periodic () {
+        io.updateInputs(inputs);
+        Logger.processInputs("Drive/Modules/" + Integer.toString(index), inputs);
     }
 }
